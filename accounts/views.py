e3dac32400ff16c8e230a3fa66631c9ba3a17dd2
Saidamir-Sh <saidamirpl@gmail.com>
2023-11-02 21:54:00 +0100

    remove exception

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 class SignupView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
