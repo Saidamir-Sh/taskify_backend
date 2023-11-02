7e25bfccf62a8cd79456b3517f4ee7a5e542094d
Saidamir-Sh <saidamirpl@gmail.com>
2023-11-02 21:49:31 +0100

    update users view

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from rest_framework import status
 
 class SignupView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
